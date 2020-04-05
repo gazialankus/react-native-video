91e71ad504047216de4950318d808139e194f6be
CHaNGeTe <1237997+CHaNGeTe@users.noreply.github.com>
2020-04-05 11:20:46 +0200

    try vualtomanager locally

---
+++ ios/Video/VualtoManager.swift
@@ -0,0 +1,31 @@
+//
+//  VualtoManager.swift
+//  Pods-VualtoDRM_Example
+//
+//  Created by Eugen Zadani on 03/04/2020.
+//
+
+import Foundation
+import UIKit
+import AVKit
+import vudrmFairPlay
+
+@objc public final class VualtoManager: NSObject {
+    
+    private override init() {
+        super.init()
+    }
+    
+    @objc public func getVualtoPlayer(assetURL: String? = nil, token: String, contentID: String) -> AVPlayer? {
+        
+        guard let unwrappedAssetURL = assetURL,
+                let assetNSURL = NSURL(string: unwrappedAssetURL),
+                let drm = vudrmFairPlay(url: assetNSURL, token: token, contentID: contentID) else {
+            return nil
+        }
+        
+        let playerItem = AVPlayerItem(asset: drm.asset)
+        return AVPlayer(playerItem: playerItem)
+    }
+    
+}
--- react-native-video.podspec
+++ react-native-video.podspec
@@ -19,7 +19,7 @@ Pod::Spec.new do |s|
   s.tvos.deployment_target = "9.0"
 
   s.subspec "Video" do |ss|
-    ss.source_files  = "ios/Video/*.{h,m}"
+    ss.source_files  = "ios/Video/*.{h,m,swift}"
     s.static_framework = true
   end
 
