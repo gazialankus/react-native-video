9e351dc67a9b2e65c2099dda3fea42fb572b6c4c
CHaNGeTe <1237997+CHaNGeTe@users.noreply.github.com>
2020-04-06 15:47:32 +0200

    playeriterm

--- ios/Video/RCTVideo.m
+++ ios/Video/RCTVideo.m
@@ -12,13 +12,14 @@
 //    [super playerItemForSource:source withCallback:handler];
     if (self->_drm != nil) {
         NSString *vualtoToken = (NSString *)[self->_drm objectForKey:@"vualtoToken"];
-        NSString *contentdId = (NSString *)[self->_drm objectForKey:@"vualtoToken"];
+        NSString *contentdId = (NSString *)[self->_drm objectForKey:@"contentdId"];
         if (vualtoToken != nil) {
-            // TODO :
+            NSString *uri = [source objectForKey:@"uri"];
             VualtoManager *vualtoManager = [[VualtoManager alloc] init];
+            AVPlayerItem *pi = [vualtoManager getVualtoPlayerItemWithAssetURL:uri token:vualtoToken contentID:contentdId];
             [super playerItemForSource:source withCallback:^(AVPlayerItem * playerItem) {
-                handler();
-            }]
+                handler(pi);
+            }];
             return;
         }
     }
