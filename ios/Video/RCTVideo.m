e14dee0a2d892b7fac062b44329d8addef0308c4
CHaNGeTe <1237997+CHaNGeTe@users.noreply.github.com>
2020-04-07 13:29:25 +0200

    original

--- ios/Video/RCTVideo.m
+++ ios/Video/RCTVideo.m
@@ -1,34 +1,4 @@
 #import "RCTVideo.h"
-#import <react_native_video/react_native_video-Swift.h>
 
-
-@implementation RCTVideo {
-    NSDictionary* _drm;
-}
-
-- (void)setDrm:(NSDictionary *)drm {
-  _drm = drm;
-}
-
-- (void)playerItemForSource:(NSDictionary *)source withCallback:(void(^)(AVPlayerItem *))handler
-{
-    NSDictionary *drm = self->_drm;
-    if (drm != nil) {
-        NSString *vualtoToken = (NSString *)[drm objectForKey:@"vualtoToken"];
-        NSString *contentdId = (NSString *)[drm objectForKey:@"contentId"];
-        if (vualtoToken != nil) {
-            NSString *uri = [source objectForKey:@"uri"];
-            VualtoManager *vualtoManager = [[VualtoManager alloc] init];
-            AVPlayerItem *pi = [vualtoManager getVualtoPlayerItemWithAssetURL:uri token:vualtoToken contentID:contentdId];
-            handler(pi);
-            return;
-        }
-    }
-    [super playerItemForSource:source withCallback:handler];    
-}
-
-- (instancetype)initWithEventDispatcher:(RCTEventDispatcher *)eventDispatcher
-{
-    return [super initWithEventDispatcher:eventDispatcher];
-}
+@implementation RCTVideo
 @end
